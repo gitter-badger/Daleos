f2792086109d07d106683da443a5446abcad4f7e
HoracioFilho <hjcf@cin.ufpe.br>
2014-12-29 16:41:22 -0200

    Muda o nome do projeto para um nome mais único

--- Daleos/My Project/AssemblyInfo.vb
+++ Daleos/My Project/AssemblyInfo.vb
@@ -2,9 +2,9 @@
 Imports System.Resources
 Imports System.Runtime.CompilerServices
 
-<Assembly: AssemblyTitle("Ed")>
-<Assembly: AssemblyProduct("Fábrica de Códigos® Ed")>
+<Assembly: AssemblyTitle("Daleos")>
+<Assembly: AssemblyProduct("Fábrica de Códigos® Daleos")>
 <Assembly: AssemblyCopyright("© Fábrica de Códigos.  All rights reserved.")>
 <Assembly: AssemblyVersion("1.0.0.*")>
 <Assembly: NeutralResourcesLanguage("pt-BR")>
-<Assembly: InternalsVisibleTo("Ed.Tests")>
+<Assembly: InternalsVisibleTo("Daleos.Tests")>
--- Daleos/Tests/My Project/AssemblyInfo.vb
+++ Daleos/Tests/My Project/AssemblyInfo.vb
@@ -1,8 +1,8 @@
 ﻿Imports System.Reflection
 Imports System.Resources
 
-<Assembly: AssemblyTitle("Tests for Ed")>
-<Assembly: AssemblyProduct("Fábrica de Códigos® Tests for Ed")>
+<Assembly: AssemblyTitle("Tests for Daleos")>
+<Assembly: AssemblyProduct("Fábrica de Códigos® Tests for Daleos")>
 <Assembly: AssemblyCopyright("© Fábrica de Códigos.  All rights reserved.")>
 <Assembly: AssemblyVersion("1.0.0.*")>
 <Assembly: NeutralResourcesLanguage("pt-BR")>
