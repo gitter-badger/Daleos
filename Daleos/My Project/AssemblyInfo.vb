0a5a6cd79402523d62d0c9d4dcc5d88f3e78a28b
HoracioFilho <hjcf@cin.ufpe.br>
2014-12-31 21:46:59 -0200

    Últimas mudanças

--- Daleos/My Project/AssemblyInfo.vb
+++ Daleos/My Project/AssemblyInfo.vb
@@ -3,8 +3,6 @@ Imports System.Resources
 Imports System.Runtime.CompilerServices
 
 <Assembly: AssemblyTitle("Daleos")>
-<Assembly: AssemblyProduct("Fábrica de Códigos® Daleos")>
-<Assembly: AssemblyCopyright("© Fábrica de Códigos.  All rights reserved.")>
 <Assembly: AssemblyVersion("1.0.0.*")>
 <Assembly: NeutralResourcesLanguage("pt-BR")>
 <Assembly: InternalsVisibleTo("Daleos.Tests")>
--- Daleos/Tests/My Project/AssemblyInfo.vb
+++ Daleos/Tests/My Project/AssemblyInfo.vb
@@ -2,7 +2,5 @@
 Imports System.Resources
 
 <Assembly: AssemblyTitle("Tests for Daleos")>
-<Assembly: AssemblyProduct("Fábrica de Códigos® Tests for Daleos")>
-<Assembly: AssemblyCopyright("© Fábrica de Códigos.  All rights reserved.")>
 <Assembly: AssemblyVersion("1.0.0.*")>
 <Assembly: NeutralResourcesLanguage("pt-BR")>
